1564fb9c670754c1c110c45a26fe08dbc56ec07c
Jérôme Martin <merdelin.mortetombe@laposte.net>
2017-04-21 14:36:59 +0200

    Add a first draft of the user interface using pygubu

---
+++ fxpqeditor/editor.py
@@ -0,0 +1,77 @@
+#/usr/bin/env python3
+
+import tkinter as tk
+from tkinter import ttk, filedialog
+
+import pygubu
+
+"""
+TODO:
+- New menu is populated with standard types (zon, npc, item, enemy...)
+- Save and Save as menu
+- Opening files creates tabs
+- Custom tabs with a close button
+- Solution tree / Untracked tree
+- New files saved inside the dimension folder get tracked
+- Right click on untracked file -> Include in dimension
+
+Text editor:
+- Line numbers
+- Text highlighting
+- Right click copy/cut/paste
+- Search & replace
+- Display validation errors
+- Show matching tags
+- Autocomplete class and properties
+"""
+
+class Application(pygubu.TkApplication):
+    def _create_ui(self):
+        self.builder = builder = pygubu.Builder()
+
+        builder.add_from_file('editor.ui')
+
+        self.mainwindow = builder.get_object('Frame_Main', self.master)
+        self.mainwindow.pack(fill=tk.BOTH, expand=1)
+        self.panedwindow = builder.get_object('Panedwindow_Main', self.master)
+        self.panedwindow.pack(fill=tk.BOTH, expand=1)
+
+        self.texteditor = builder.get_object('Text_Editor', self.master)
+
+        self.mainmenu = menu = builder.get_object('Menu_Main', self.master)
+        self.set_menu(menu)
+
+        builder.connect_callbacks(self)
+
+    def on_open(self):
+        filepath = filedialog.askopenfilename(
+            filetypes = (
+                ("FXPQ file", "*.fxpq"),
+                ("All files", "*.*")))
+        if not filepath:
+            return
+
+        with open(filepath) as f:
+            text = f.read()
+            self.texteditor.delete("1.0", tk.END)
+            self.texteditor.insert(tk.END, text)
+
+    def on_quit(self):
+        self.quit()
+
+    def on_about(self):
+        about = self.builder.get_object('Dialog_About', self.master)
+        buttonclose = self.builder.get_object('Button_AboutClose', self.master)
+
+        def on_close_about():
+            about.close()
+
+        buttonclose.config(command=on_close_about)
+        about.run()
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("FXPQuest Editor")
+    app = Application(root)
+    app.run()
